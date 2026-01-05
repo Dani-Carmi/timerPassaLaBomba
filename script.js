0c6bcb351d71b27cfbf4e4722d13075d424816c2
Dani-Carmi <danielecarminati.dc@gmail.com>
2026-01-05 16:46:24 +0100

    feat: add images, removed loop and use longer audio instead

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 const tickingAudio = new Audio("./audio/ticking.mp3");
 const explodingAudio = new Audio("./audio/explosion.mp3");
 const playButton = document.getElementById("button");
-
-tickingAudio.loop = true;
-
-audio_file.addEventListener("timeupdate", function () {
-  var buffer = 0.44;
-  if (this.currentTime > this.duration - buffer) {
-    this.currentTime = 0;
-    this.play();
-  }
-});
+const restartButton = document.getElementById("restart");
+const bombImage = document.getElementById("bomb-image");
+const blastImage = document.getElementById("blast-image");
 
 function getRandomDuration(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -20,8 +13,10 @@ function play() {
   playButton.disabled = true;
 
   tickingAudio.play();
+  bombImage.style.display = "block";
+  playButton.style.display = "none";
 
-  const duration = getRandomDuration(15, 60) * 1000;
+  const duration = getRandomDuration(15, 45) * 1000;
   console.log(`Bomb will explode in ${duration / 1000} seconds`);
 
   setTimeout(() => {
@@ -29,12 +24,21 @@ function play() {
     tickingAudio.currentTime = 0;
     explodingAudio.play();
 
+    blastImage.style.display = "block";
+    bombImage.style.display = "none";
+
     explodingAudio.addEventListener(
       "ended",
       () => {
-        playButton.disabled = false;
+        restartButton.style.display = "block";
       },
       { once: true }
     );
   }, duration);
+}
+
+function restart() {
+  playButton.disabled = false;
+  blastImage.style.display = "none";
+  playButton.style.display = "block";
 }
